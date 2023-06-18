ad92317d81f410f7fda23fffc299e0594f53ca40
josephsanchez13 <imjosephsanchez13@gmail.com>
2023-06-19 00:56:29 +0800

    added authentication + created raw sql queries

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    //return view('welcome');
+   // $users = DB::select("select * from users");
+
+    // $user = DB::insert('insert into users (name, email, password) value (?,?,?)',[
+    //     'Sarthak',
+    //     'sarthak12@bitfumes.com',
+    //     'password',
+    // ]);
+
+    //$user = DB::update("update users set email='abc@bitfumes.com' where id=3");
+
+    //$user = DB::delete("delete from users where id=3");
+    //dd($users);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
